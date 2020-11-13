44c8f4a05bee43c8022793604f0d21e3bb000fb5
JericHunter <jeric.hunterr@gmail.com>
2020-11-13 03:39:32 -0500

    psuedo

--- backend/pdf.py
+++ backend/pdf.py
@@ -4,25 +4,27 @@
 
 # from https://www.geeksforgeeks.org/convert-text-and-text-file-to-pdf-using-python/
 from fpdf import FPDF
-  
-# save FPDF() class into a  
-# variable pdf 
-pdf = FPDF() 
-  
-# Add a page 
-pdf.add_page() 
-  
-# set style and size of font  
-# that you want in the pdf 
-pdf.set_font("Arial", size = 15) 
-  
-# create a cell 
-pdf.cell(200, 10, txt = "This is a Test for Team Porculion",  
-         ln = 1, align = 'C') 
-  
-# add another cell 
-pdf.cell(200, 10, txt = "This code works and can be used.", 
-         ln = 2, align = 'C') 
-  
-# save the pdf with name .pdf 
+
+# save FPDF() class into a
+# variable pdf
+pdf = FPDF()
+
+# Add a page
+pdf.add_page()
+
+# set style and size of font
+# that you want in the pdf
+pdf.set_font("Arial", size = 15)
+
+# create a cell
+pdf.cell(200, 10, txt = "This is a Test for Team Porculion",
+         ln = 1, align = 'C')
+
+#going to create a var that uses a text file for pdf
+
+# add another cell
+pdf.cell(200, 10, txt = "This code works and can be used.",
+         ln = 2, align = 'C')
+
+# save the pdf with name .pdf
 pdf.output("GFG.pdf")
