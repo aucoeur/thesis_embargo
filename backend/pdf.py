9b4e9bac78ce6e07ecb2d384a1f35177375fd455
JericHunter <jeric.hunterr@gmail.com>
2020-11-13 03:54:13 -0500

    small bug

--- backend/pdf.py
+++ backend/pdf.py
@@ -21,10 +21,11 @@ pdf.set_font("Arial", size = 15)
 #          ln = 1, align = 'C')
 
 #going to create a var that uses a text file for pdf
-file = open("Template_testing.text","r")
+file = open("Template_testing.txt","r")
 #iterate over text file to insert into pdf
 for i in file:
-    
+    pdf.cell(200, 10, txt = i,
+         ln = 1, align = 'C')
 # add another cell
 # pdf.cell(200, 10, txt = "This code works and can be used.",
 #          ln = 2, align = 'C')
