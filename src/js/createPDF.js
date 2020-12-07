e497a144705997923d702c1f506ec075f2c47869
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-12-06 23:31:21 -0800

    create sample pdf from blob

--- src/js/createPDF.js
+++ src/js/createPDF.js
@@ -16,12 +16,27 @@ const createPDF = () => {
     PropertiesService.getDocumentProperties().getProperty('TEMP_FOLDER_ID')
   );
 
+  const pdfFolder = DriveApp.getFolderById(
+    PropertiesService.getDocumentProperties().getProperty('PDF_FOLDER_ID')
+  );
+
   const placeholder = template.makeCopy(tempFolder);
   const tempFile = DocumentApp.openById(placeholder.getId());
   const body = tempFile.getBody();
   body.replaceText('{student_name}', studentName);
   body.replaceText('{thesis_title}', thesisTitle);
   tempFile.saveAndClose();
+
+  // Convert tempFile to PDF
+  const pdfBlob = tempFile.getAs(MimeType.PDF);
+  // eslint-disable-next-line no-unused-vars
+  const [WEEK, MM, DD, YYYY] = new Date().toDateString('en-US').split(' ');
+  pdfFolder
+    .createFile(pdfBlob)
+    .setName(`Embargo Exception Request - ${studentName}-${YYYY}-${MM}-${DD}`);
+
+  // Move temp file to Trash
+  Drive.Files.remove(placeholder.getId());
 };
 
 export default createPDF;
